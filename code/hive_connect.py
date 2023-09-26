59dbd99c8c014728554c4c6b9534b0a8232c1a52
rp926463 <roshanpatil1703@gmail.com>
2023-09-26 17:36:48 +0530

    test2

--- code/hive_connect.py
+++ code/hive_connect.py
@@ -79,3 +79,54 @@ results = cursor.fetchall()
 
 # Close the connection when done
 conn.close()
+
+
+
+
+
+
+
+
+
+
+from pyhive import hive
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift_sasl import TSaslClientTransport
+
+# Set the connection parameters
+hive_host = "your_hive_host"
+hive_port = your_hive_port
+hive_principal = "your_hive_principal"  # Example: "hive/_HOST@REALM"
+hive_database = "your_hive_database"  # Replace with your database name
+
+# Create a connection to Hive
+transport = TSocket.TSocket(hive_host, hive_port)
+transport = TTransport.TBufferedTransport(transport)
+
+# Set up the SASL client
+sasl_client = TSaslClientTransport(
+    transport,
+    mechanism='GSSAPI',  # Kerberos
+    username=hive_principal,
+    service="hive",  # Hive service name
+)
+
+# Open the transport
+sasl_client.open()
+
+# Establish the Hive connection
+conn = hive.Connection(
+    host=hive_host,
+    port=hive_port,
+    auth=sasl_client,
+    database=hive_database,
+)
+
+# Execute Hive queries
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM your_hive_table")
+results = cursor.fetchall()
+
+# Close the connection when done
+conn.close()
