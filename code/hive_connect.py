fcdc5450755650547f1e1723413fc124817e04a1
rp926463 <roshanpatil1703@gmail.com>
2023-09-26 17:40:18 +0530

    test3

--- code/hive_connect.py
+++ code/hive_connect.py
@@ -130,3 +130,63 @@ results = cursor.fetchall()
 
 # Close the connection when done
 conn.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+from pyhive import hive
+from thrift.transport import TTransport
+from thrift.transport.TSocket import TSocket
+from thrift_sasl import TSaslClientTransport
+
+# Set the connection parameters
+hive_host = "your_hive_host"
+hive_port = your_hive_port
+hive_principal = "your_hive_principal"  # Example: "hive/_HOST@REALM"
+hive_database = "your_hive_database"  # Replace with your database name
+
+# Set the KERBEROS_TICKET_CACHE environment variable
+os.environ['KERBEROS_TICKET_CACHE'] = '/tmp/krb5cc_1000'  # Replace with your ticket cache path
+
+# Create a connection to Hive
+transport = TSocket(hive_host, hive_port)
+transport = TTransport.TBufferedTransport(transport)
+
+# Open the transport
+transport.open()
+
+# Create a SASL client for Kerberos authentication
+sasl_client = TSaslClientTransport(
+    transport,
+    mechanism='GSSAPI',
+    sasl_protocol=None,
+    service="hive",  # Hive service name
+)
+
+# Establish the Hive connection
+conn = hive.Connection(
+    host=hive_host,
+    port=hive_port,
+    auth=sasl_client,
+    database=hive_database,
+)
+
+# Execute Hive queries
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM your_hive_table")
+results = cursor.fetchall()
+
+# Close the connection when done
+conn.close()
