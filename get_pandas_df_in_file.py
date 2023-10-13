2b63855f50dea16f78ed0666203b0ee628cd87c7
rp926463 <roshanpatil1703@gmail.com>
2023-10-13 09:44:00 +0530

    new code added

---
+++ get_pandas_df_in_file.py
@@ -0,0 +1,48 @@
+Approach 1:
+
+To write a Pandas DataFrame to a file with all data enclosed in double quotes, you can use the quoting parameter in the to_csv method. Setting quoting=csv.QUOTE_ALL will enclose all data in double quotes, which is a safe approach to avoid conflicts with any single-character delimiter.
+
+	import pandas as pd
+	import csv
+
+	# Sample DataFrame
+	data = {
+		'Name': ['John', 'Alice', 'Bob'],
+		'Age': [30, 25, 35],
+		'City': ['New York', 'Los Angeles', 'Chicago']
+	}
+
+	df = pd.DataFrame(data)
+	delimiter = '|'
+
+	df.to_csv('data_with_double_quotes.txt', sep=delimiter, index=False, quoting=csv.QUOTE_ALL)
+
+	Output:
+	"Name"|"Age"|"City"
+	"John"|"30"|"New York"
+	"Alice"|"25"|"Los Angeles"
+	"Bob"|"35"|"Chicago"
+	
+
+Approach 2 (Multi-character delimiter):
+	import pandas as pd
+
+	data = {
+		'Name': ['John', 'Alice', 'Bob'],
+		'Age': [30, 25, 35],
+		'City': ['New York', 'Los Angeles', 'Chicago']
+	}
+
+	df = pd.DataFrame(data)
+
+	delimiter = '|#'
+
+	with open('data_with_multi_delimiter.txt', 'w') as f:
+		for index, row in df.iterrows():
+			f.write(delimiter.join([str(item) for item in row]) + '\n')
+
+
+	Output:
+	John|#30|#New York
+	Alice|#25|#Los Angeles
+	Bob|#35|#Chicago
